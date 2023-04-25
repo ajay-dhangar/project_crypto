8df27fa0350e2e086205ae92b649d67c553e994a
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-04-25 22:40:12 +0530

    added new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navbar } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="hello">Hello World!</h1>
+      <div>
+        <Navbar />
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <h3>Navebar</h3>
+    </div>
+  );
+}
+
+export default Navbar;
